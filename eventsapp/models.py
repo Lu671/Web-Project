a51492aebe8f4def9c0ce861b356115ff9bf4f39
L <l@me>
2024-03-21 01:17:46 +0300

    add events model (table)

--- eventsapp/models.py
+++ eventsapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Events(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+
