5651d453cb807392530635d12ebce2f448f65120
L <l@me>
2024-03-28 11:48:05 +0300

    add month filter

--- eventsapp/views.py
+++ eventsapp/views.py
@@ -21,14 +21,22 @@ def addEvent(request):
         return render(request, 'AddEvent.html')
 
 def listEvents(request):
+    month_names = {1: 'January',2: 'February',3: 'March',4: 'April',5: 'May',
+                   6: 'June',7: 'July',8: 'August',9: 'September',10: 'October',
+                   11: 'November',12: 'December'}
     old_events = [
     {'title': 'Event 1', 'date': '2024-03-15', 'description': 'Tech workshop', 'location':'Computer College','agenda':'media\Event 1 Agenda.pdf'},
     {'title': 'Event 2', 'date': '2024-04-15', 'description': 'Programming Competition','location':'Computer College','agenda':'media\Event 2 Agenda.pdf'},
     ]
-    events = Events.objects.all()
-    context = {'events': events, 'old_events': old_events}
+    selected_month = request.GET.get('month')
+    month_name = month_names.get(int(selected_month)) if selected_month else None
+    if selected_month:
+        events = Events.objects.filter(date__month=selected_month)
+        context = {'events': events, 'month':month_name, 'old_events': old_events}
+    else:
+        events = Events.objects.all()
+        context = {'events': events, 'old_events': old_events}
     return render(request, 'EventsList.html', context)
-   
 
 
  
