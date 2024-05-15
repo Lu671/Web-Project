49dfded1d5d3e79bf147a8d86baec3dacab6e2a5
L <l@me>
2024-05-15 14:51:05 +0300

    add register\login urls

--- eventsapp/urls.py
+++ eventsapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path,include
 from eventsapp import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.home,name='home'),
     path('addEvent',views.addEvent,name='addEvent'),
     path('eventsList',views.listEvents,name='listEvents'),
     path('event/delete/<int:event_id>/', views.deleteEvent, name='delete_event'),
+     path('login/', views.login_page, name='login'),    # Login page
+    path('register/', views.register_page, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('events/<int:event_id>/register/', views.register_event, name='register_event'),
+    path('registered_events/events', views.registered_events, name='registered_events'),
 ]
