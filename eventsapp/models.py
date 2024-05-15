4ef1c1d163724b18149c00aeac4fdcb97a7d702e
L <l@me>
2024-05-15 14:50:34 +0300

    add registered events

--- eventsapp/models.py
+++ eventsapp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Events(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField()
     description = models.TextField()
     location = models.CharField(max_length=100)
+    registered_users = models.ManyToManyField(User, related_name='registered_events')
 
